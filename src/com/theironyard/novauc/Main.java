bbce8519afc9a34ce818fab4af93fb998d5f1d95
JacobP <JacobP@macusermacbook6.nvstu.edu>
2017-02-08 09:57:48 -0500

    updated methods

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -20,40 +20,47 @@ public class Main {
         System.out.println("Please enter year produced: ");
         System.out.println("Please enter total runtime: ");
 
+        {
+            String name = "Jacob";
+            int age = 25;
+            double height = 67, weight = 180;
+            boolean isMarried = true;
+
+
+            System.out.println("Please enter name: " + name);
+            System.out.println("Please enter age: " + age);
+            System.out.println("Please enter height: " + height);
+            System.out.println("Please enter weight: " + weight);
+            System.out.println("Is person married: " + isMarried);
+        }
+        {
+            String name = "name";
+            int numForklift = 5, numDoors = 8, numBins = 125, numMaterials = 1200;
+
+            System.out.println("Please enter name: " + name);
+            System.out.println("Please enter number of forklifts: " + numForklift);
+            System.out.println("Please enter number of doors: " + numDoors);
+            System.out.println("Please enter number of bins: " + numBins);
+            System.out.println("Please enter number of materials: " + numMaterials);
+        }
+
+        {
+            int distance = 75;
+            double mpg = 20, pricePerGallon = 1.25, lodgingCost = 95, totalCost = 116.25;
+
+            System.out.println("Please enter distance: " + distance);
+            System.out.println("Please enter number of mpg: " + mpg);
+            System.out.println("Please enter price for pricePerGallon: " + pricePerGallon);
+            System.out.println("Please enter price for lodgingCost: " + lodgingCost);
+            System.out.println("Please enter totalCost: " + totalCost);
+
+
+
+
+
     }
 
-    {
-        String name;
-        int age;
-        double height, weight;
-        boolean isMarried;
 
-        System.out.println("Please enter name: ");
-        System.out.println("Please enter age: ");
-        System.out.println("Please enter height: ");
-        System.out.println("Please enter weight: ");
-        System.out.println("Is person married: ");
-    }
-    {
-        String name;
-        int numForklift, numDoors, numBins, numMaterials;
-
-        System.out.println("Please enter name: ");
-        System.out.println("Please enter number of forklifts: ");
-        System.out.println("Please enter number of doors: ");
-        System.out.println("Please enter number of bins: ");
-        System.out.println("Please enter number of materials: ");
-    }
-
-    {
-        int distance;
-        double mpg, pricePerGallon, lodgingCost, totalCost;
-
-        System.out.println("Please enter name: ");
-        System.out.println("Please enter number of forklifts: ");
-        System.out.println("Please enter number of doors: ");
-        System.out.println("Please enter number of bins: ");
-        System.out.println("Please enter number of materials: ");
     }
     }
 
